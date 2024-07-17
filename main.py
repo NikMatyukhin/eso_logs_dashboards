217217a565740f436758227957afab85ae1e6185
NikMatyukhin <nmatryukhin@mail.ru>
2024-07-18 05:58:16 +0700

    feat(di): Using new container with app cli-startup

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import logging
 from datetime import datetime
 
 import click
-import dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from src.app import create_app
-from src.core.di import configure_inject
+from src.core.di import create_container
 from src.tasks import load_daily_reports_task, sync_abilities_task, sync_items_task
 
 
@@ -17,31 +17,38 @@ def cli() -> None:
 @cli.command()
 @click.option("--background", is_flag=True, show_default=True, default=False)
 def start(background: bool) -> None:
-    if background:
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(load_daily_reports_task, "cron", hour="0")
-        scheduler.start()
+    container = create_container()
+    with container.sync_context():
+        if background:
+            scheduler = BackgroundScheduler()
+            scheduler.add_job(load_daily_reports_task, "cron", hour="0")
+            scheduler.start()
     app = create_app()
     app.run_server(debug=True)
 
 
 @cli.command()
 def sync_items() -> None:
-    sync_items_task()
+    container = create_container()
+    with container.sync_context():
+        sync_items_task()  # type: ignore
 
 
 @cli.command()
 def sync_abilities() -> None:
-    sync_abilities_task()
+    container = create_container()
+    with container.sync_context():
+        sync_abilities_task()  # type: ignore
 
 
 @cli.command()
 @click.option("--day", type=click.DateTime(formats=["%Y-%m-%d"]), default=None)
 def load_reports(day: datetime | None) -> None:
-    load_daily_reports_task(day=day)
+    container = create_container()
+    with container.sync_context():
+        load_daily_reports_task(day=day)  # type: ignore
 
 
 if __name__ == "__main__":
-    dotenv.load_dotenv(".env")
-    configure_inject()
+    logging.basicConfig(level="INFO")
     cli()
