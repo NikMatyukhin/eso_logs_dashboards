989da788314091a107a44f1f3d71799cb3ecdbca
NikMatyukhin <nmatryukhin@mail.ru>
2024-07-18 05:52:58 +0700

    fix(mypy): Change generic annotation in transform

--- src/core/transform_utils.py
+++ src/core/transform_utils.py
@@ -15,7 +15,7 @@ def transform_to_id_map(
 def transform_to_id_seq_map(
     models: list[TModel], ids: KeysView[int] | None = None
 ) -> Mapping[int, Sequence[TModel]]:
-    id_seq_map: Mapping[int, list] = defaultdict(list)
+    id_seq_map: Mapping[int, list[TModel]] = defaultdict(list)
     for model in models:
         if ids is None or model.id in ids:
             id_seq_map[model.id].append(model)
